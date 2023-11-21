654a0c65a328606c163f7de139b31d76eef61831
mariamf1985 <mariamf1985@gmail.com>
2023-11-21 14:28:28 +0100

    variables created

---
+++ variables.js
@@ -0,0 +1,35 @@
+/*NUMBERS*/
+
+const pi = 3.1416;
+let number1 = 25;
+let number2 = 10;
+
+console.log(pi);
+console.log(number1);
+console.log(number2);
+
+/*STRINGS*/
+
+let name = "María";
+let surname = "Méndez";
+let pet = "cat";
+
+console.log(name);
+console.log(surname);
+console.log(pet);
+
+/*ARRAYS*/
+
+let vegetables = ["onion", "carrot", "lettuce"];
+let fruits = ["apple", "strawberry", "banana"];
+
+console.log(vegetables); 
+console.log(fruits);
+
+/*BOOLEANS*/
+
+var esVerdadero = true;
+var esFalso = false;
+
+console.log(esVerdadero); 
+console.log(esFalso);     
