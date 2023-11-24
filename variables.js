fd1e44677c991e59f0821f3bf6b817a1f98a327d
mariamf1985 <mariamf1985@gmail.com>
2023-11-24 11:53:51 +0100

    variables added

--- variables.js
+++ variables.js
@@ -1,35 +1,74 @@
-/*NUMBERS*/
+let numbers = "NUMBERS";
 
 const pi = 3.1416;
 let number1 = 25;
 let number2 = 10;
 
+document.write(numbers);
 console.log(pi);
+document.write(pi);
 console.log(number1);
+document.write(number1);
 console.log(number2);
+document.write(number2);
 
-/*STRINGS*/
+
+let strings = "STRINGS";
 
 let name = "María";
 let surname = "Méndez";
 let pet = "cat";
 
+document.write(strings);
 console.log(name);
+document.write(name);
 console.log(surname);
+document.write(surname);
 console.log(pet);
+document.write(pet);
 
-/*ARRAYS*/
+
+let arrays = "ARRAYS";
 
 let vegetables = ["onion", "carrot", "lettuce"];
 let fruits = ["apple", "strawberry", "banana"];
 
-console.log(vegetables); 
+document.write(arrays);
+console.log(vegetables);
+document.write(vegetables);
 console.log(fruits);
+document.write(fruits);
 
-/*BOOLEANS*/
+
+let booleans = "BOOLEANS";
 
 var esVerdadero = true;
 var esFalso = false;
 
-console.log(esVerdadero); 
-console.log(esFalso);     
+document.write(booleans);
+console.log(esVerdadero);
+document.write(esVerdadero);
+console.log(esFalso);
+document.write(esFalso);
+
+
+let addition = number1 + number2;
+console.log(addition);
+document.write(addition);
+
+let substraction = number1 - number2;
+console.log(substraction);
+document.write(substraction);
+
+let multiplication = number1 * number2;
+console.log(multiplication);
+document.write(multiplication);
+
+let division = number1 / number2;
+console.log(division);
+document.write(division);
+
+let sentence = `My name is ${name} ${surname} and my pet is a ${pet}.`
+
+console.log(sentence);
+document.write(sentence);
