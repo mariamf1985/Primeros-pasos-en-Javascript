47ecad7e4024f203b50269a49079f09aca9d1eb7
mariamf1985 <mariamf1985@gmail.com>
2023-11-24 12:08:25 +0100

    functions added

---
+++ functions.js
@@ -0,0 +1,15 @@
+function greeting(){
+    let name = "María";
+    return `Hola ${name}.`;
+}
+
+console.log(greeting());
+document.write(greeting());
+
+function add() {
+    let number1 = 20;
+    let number2 = 15;
+    return (number1+number2)
+}
+console.log(add());
+document.write(add());
