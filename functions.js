4ec316e9da1896aafaec969bee6a8b38750954ea
mariamf1985 <mariamf1985@gmail.com>
2023-12-29 15:15:12 +0100

    variables

--- functions.js
+++ functions.js
@@ -1,15 +1,30 @@
-function greeting(){
+function greeting() {
     let name = "María";
     return `Hola ${name}.`;
 }
 
 console.log(greeting());
-document.write(greeting());
+document.write(greeting() + "<br>" + "<br>");
 
 function add() {
     let number1 = 20;
     let number2 = 15;
-    return (number1+number2)
+    return (number1 + number2)
 }
 console.log(add());
-document.write(add());
+document.write(add() + "<br>" + "<br>");
+
+
+function parity(number) {
+    if (number % 2 === 0) {
+        return number + " is an even number";
+    } else {
+        return number + " is an odd number"
+
+    }
+}
+
+console.log (parity(5));
+document.write (parity(5) + "<br>");
+console.log (parity(6));
+document.write (parity(6) + "<br>");
--- variables.js
+++ variables.js
@@ -1,72 +1,72 @@
-let numbers = "NUMBERS";
+let numbers = "<strong>NUMBERS</strong>";
 
 const pi = 3.1416;
 let number1 = 25;
 let number2 = 10;
 
-document.write(numbers);
+document.write(numbers + "<br>");
 console.log(pi);
-document.write(pi);
+document.write(pi + "<br>");
 console.log(number1);
-document.write(number1);
+document.write(number1 + "<br>");
 console.log(number2);
-document.write(number2);
+document.write(number2 + "<br>" + "<br>");
 
 
-let strings = "STRINGS";
+let strings = "<strong>STRINGS</strong>";
 
 let name = "María";
 let surname = "Méndez";
 let pet = "cat";
 
-document.write(strings);
+document.write(strings+ "<br>");
 console.log(name);
-document.write(name);
+document.write(name + "<br>");
 console.log(surname);
-document.write(surname);
+document.write(surname + "<br>");
 console.log(pet);
-document.write(pet);
+document.write(pet + "<br>" + "<br>");
 
 
-let arrays = "ARRAYS";
+let arrays = "<strong>ARRAYS</strong>";
 
 let vegetables = ["onion", "carrot", "lettuce"];
 let fruits = ["apple", "strawberry", "banana"];
 
-document.write(arrays);
+document.write(arrays + "<br>");
 console.log(vegetables);
-document.write(vegetables);
+document.write(vegetables + "<br>");
 console.log(fruits);
-document.write(fruits);
+document.write(fruits + "<br>" + "<br>");
 
 
-let booleans = "BOOLEANS";
+let booleans = "<strong>BOOLEANS</strong>";
 
 var esVerdadero = true;
 var esFalso = false;
 
-document.write(booleans);
+document.write(booleans + "<br>");
 console.log(esVerdadero);
-document.write(esVerdadero);
+document.write(esVerdadero + "<br>");
 console.log(esFalso);
-document.write(esFalso);
+document.write(esFalso + "<br>" + "<br>");
 
 
 let addition = number1 + number2;
 console.log(addition);
-document.write(addition);
+document.write(addition + "<br>");
 
 let substraction = number1 - number2;
 console.log(substraction);
-document.write(substraction);
+document.write(substraction + "<br>");
 
 let multiplication = number1 * number2;
 console.log(multiplication);
-document.write(multiplication);
+document.write(multiplication + "<br>");
 
 let division = number1 / number2;
 console.log(division);
-document.write(division);
+document.write(division + "<br>" + "<br>");
 
 let sentence = `My name is ${name} ${surname} and my pet is a ${pet}.`
 
